19591d9c0c1ac18ae7473e382541f21dfd2ba491
ExMachinery <milkcarrotc@gmail.com>
2026-03-18 02:45:06 +0500

    Add method to identify possible paths for current knight position

--- board.rb
+++ board.rb
@@ -2,21 +2,42 @@ require_relative 'square'
 class Chessboard
   attr_accessor :board
   def initialize
-  reference = [
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7],
-    [0, 1, 2, 3, 4, 5, 6, 7]
-  ]
-
-  @board = []
+    puts "hey!"
   end
+
+
+  def knight_moves(location, target)
+
+    # Take target as a direction rule for x and y
+    # for this position
+    
+  end
+
+  def get_allowed_squares(location)
+    x, y = location[0], location[1]
+    array_of_squares = []
+    [2, -2].each do |i|
+      [1, -1].each do |j|
+        a, b = x + i, y + j
+        array_of_squares << [a, b] if a >= 0 && b >= 0 && a <= 7 && b <= 7
+        a, b = y + i, x + j
+        array_of_squares << [a, b] if a >= 0 && b >= 0 && a <= 7 && b <= 7
+      end
+    end
+    array_of_squares
+    
+  end
+
 end
 
+# x - 2; y - 1
+# x - 2; y + 1
+# x + 2; y + 1
+# x + 2; y - 1
+# x - 1; y - 2
+# x - 1; y + 2
+# x + 1; y + 2
+# x + 1; y - 2
 
 
 
@@ -25,25 +46,25 @@ end
 
 
 # Example of the BFS from previous task
-  def level_order
-    return to_enum(:level_order) unless block_given?
+  # def level_order
+  #   return to_enum(:level_order) unless block_given?
 
-    result = []
-    @queue << @root
-    until @queue.empty?
-      result << queue_handler
-    end
+  #   result = []
+  #   @queue << @root
+  #   until @queue.empty?
+  #     result << queue_handler
+  #   end
 
-    result.each do |val|
-      yield(val)
-    end
-    self
-  end
+  #   result.each do |val|
+  #     yield(val)
+  #   end
+  #   self
+  # end
 
-  def queue_handler
-    node = @queue.shift
-    result = node.value
-    @queue.push(node.left) if node.left
-    @queue.push(node.right) if node.right
-    result
-  end
+  # def queue_handler
+  #   node = @queue.shift
+  #   result = node.value
+  #   @queue.push(node.left) if node.left
+  #   @queue.push(node.right) if node.right
+  #   result
+  # end
